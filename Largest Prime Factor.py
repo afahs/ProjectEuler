5d607a7ce9f8dc54c4101d8b65514b09e1f8e9e6
afahs <adam.fahs@gmail.com>
2017-08-13 00:03:03 -0400

    Question3
    project Euler question 3

---
+++ Largest Prime Factor.py
@@ -0,0 +1,13 @@
+n = 600851475143
+newn = n
+largestFact = 0
+counter = 2
+while(counter*counter<=newn):
+    if(newn%counter ==0):
+        newn = newn / counter
+        largestFact = counter
+    else:
+        counter = counter+1
+if(newn>largestFact):
+    largestFact = newn
+print(largestFact)
